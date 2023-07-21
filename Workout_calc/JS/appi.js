6f54125feb22440f506f80624a9e55b8148eba00
Pro100Vlad1047 <91087864+Pro100Vlad1047@users.noreply.github.com>
2023-07-22 02:18:12 +0300

    Add files via upload

--- Workout_calc/JS/appi.js
+++ Workout_calc/JS/appi.js
@@ -1,25 +1,32 @@
 let set =document.getElementById('set'),
     sets=document.getElementById('sets'),
-    reps=document.getElementById('reps');
+    reps=document.getElementById('reps'),
+    steps=document.getElementById('steps'),
+    stepsInfo=document.getElementById('stepsInfo');
 
 
-const totalReps=(e)=>{
-  if(e==1){
-    return 1;
+const totalReps=(e,step=1)=>{
+  console.log(step);
+  if(e%step!=0){return `'(step number incorrect)'`}
+  if(e==step){
+    return step;
   }else{
-    return e+totalReps(e-1);
+    return e+totalReps(e-step,step);
   }
 }
 
+const output=(e)=>{
 
-set.addEventListener('input',(e)=>{
-  console.log(e.target.value);
-  if(e.target.value!=''&& e.target.value<9999&& e.target.value>0){
-    sets.innerHTML=`'${e.target.value}'`;
-    reps.innerHTML=`'${totalReps(Number(e.target.value))}'`;
+  if(set.value!=''&& set.value<10000 && set.value>0){
+    sets.innerHTML=`'${set.value}'`;
+    reps.innerHTML=`'${totalReps(Number(set.value),Number(steps.value))}'`;
+    stepsInfo.innerHTML=`'${steps.value}'`;
   }else{
     sets.innerHTML=`'(set the number)'`;
     reps.innerHTML=`'(set the number)'`;
+    stepsInfo.innerHTML=`'(set the number)'`;
   }
+}
 
-})
+set.addEventListener('input',output);
+steps.addEventListener('input',output);
